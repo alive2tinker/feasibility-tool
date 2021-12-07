cf9344538ac5de0024aefa8fb41c6d2ffa63b61e
Abdulmalik Alsufayran <alsufiran@gmail.com>
2021-12-08 00:35:39 +0300

    partially implemented pdf

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -531,17 +531,13 @@
                 </h4>
               </div>
               <div class="col-md-4">
-                <h4>
-                  {{ `${PRA} ${accountingOptions.currency}` }}<br /><small
-                    >معدل ربح سنوي</small
-                  >
+                <h4 class="text-center">
+                  -<br /><small>فترة استرداد رأس المال </small>
                 </h4>
               </div>
               <div class="col-md-4">
-                <h4>
-                  {{ `${PRA} ${accountingOptions.currency}` }}<br /><small
-                    >معدل ربح سنوي</small
-                  >
+                <h4 class="text-center">
+                  -<br /><small>معدل الحصة السوقية </small>
                 </h4>
               </div>
             </div>
@@ -551,81 +547,65 @@
             <div class="row">
               <div class="col-md-4">
                 <h4>
-                  {{ `${PRA} ${accountingOptions.currency}` }}<br /><small
-                    >معدل ربح سنوي</small
-                  >
+                  {{ `${capital} ${accountingOptions.currency}` }}<br /><small
+                    >رأس مال المشروع
+                  </small>
                 </h4>
               </div>
               <div class="col-md-4">
                 <h4>
-                  {{ `${PRA} ${accountingOptions.currency}` }}<br /><small
-                    >معدل ربح سنوي</small
-                  >
+                  {{ `${debtsFinanced} ${accountingOptions.currency}`
+                  }}<br /><small>مديونية لتمويل رأس المال </small>
                 </h4>
               </div>
               <div class="col-md-4">
                 <h4>
-                  {{ `${PRA} ${accountingOptions.currency}` }}<br /><small
-                    >معدل ربح سنوي</small
-                  >
+                  {{ `${personalFinance} ${accountingOptions.currency}`
+                  }}<br /><small>تمويل خاص للمشروع </small>
                 </h4>
               </div>
             </div>
           </b-card-body>
           <b-card-body>
             <b-card-title>الربحية السنوية</b-card-title>
-            <div class="row">
-              <div class="col-md-4">
-                <h4>
-                  {{ `${PRA} ${accountingOptions.currency}` }}<br /><small
-                    >معدل ربح سنوي</small
-                  >
-                </h4>
-              </div>
-              <div class="col-md-4">
-                <h4>
-                  {{ `${PRA} ${accountingOptions.currency}` }}<br /><small
-                    >معدل ربح سنوي</small
-                  >
-                </h4>
-              </div>
-              <div class="col-md-4">
-                <h4>
-                  {{ `${PRA} ${accountingOptions.currency}` }}<br /><small
-                    >معدل ربح سنوي</small
-                  >
-                </h4>
-              </div>
-            </div>
+            <table class="table w-100">
+              <thead>
+                <th>السنة المالية</th>
+                <th>
+                  <p>التكاليف السنوية <br />(الشهرية) <br />(٪ من الإيرادات)</p>
+                </th>
+                <th>
+                  <p>الارباح السنوية <br />(الشهرية) <br />(٪ من الإيرادات)</p>
+                </th>
+              </thead>
+              <tbody></tbody>
+            </table>
           </b-card-body>
           <b-card-body>
             <b-card-title>الية التسعير بالتكلفة</b-card-title>
-            <div class="row">
-              <div class="col-md-4">
-                <h4>
-                  {{ `${PRA} ${accountingOptions.currency}` }}<br /><small
-                    >معدل ربح سنوي</small
-                  >
-                </h4>
-              </div>
-              <div class="col-md-4">
-                <h4>
-                  {{ `${PRA} ${accountingOptions.currency}` }}<br /><small
-                    >معدل ربح سنوي</small
-                  >
-                </h4>
-              </div>
-              <div class="col-md-4">
-                <h4>
-                  {{ `${PRA} ${accountingOptions.currency}` }}<br /><small
-                    >معدل ربح سنوي</small
-                  >
-                </h4>
-              </div>
-            </div>
+            <table class="table w-100">
+              <thead>
+                <th>المنتج</th>
+                <th>أدنى سعر للربحية</th>
+                <th>الهامش الربحي</th>
+              </thead>
+              <tbody>
+                <tr v-for="(product, index) in products" :key="index">
+                  <td>{{ product.name }}</td>
+                  <td>{{ product.price }}</td>
+                  <td>{{ `${getProfitMargin(product)} %` }}</td>
+                </tr>
+                <h5 v-if="products.length === 0" class="mt-4">
+                  لايوجد بيانات حتى الان
+                </h5>
+              </tbody>
+            </table>
           </b-card-body>
         </b-card>
-        <button @click="generateReport" class="btn btn-primary btn-block btn-lg my-4">
+        <button
+          @click="generateReport"
+          class="btn btn-primary btn-block btn-lg my-4"
+        >
           حفظ الدراسة
         </button>
         <vue-html2pdf
@@ -648,29 +628,163 @@
           <section slot="pdf-content">
             <!-- PDF Content Here -->
             <div class="container-fluid">
-              <div class="d-flex justify-content-between border-bottom py-4">
-              <div>
+              <div class="d-flex justify-content-between py-4">
                 <h1>دراسة جدوى</h1>
-                اخر تحديث في {{ timestamp }}
+                <h1>نظام منشات</h1>
               </div>
-              <h1>نظام منشات</h1>
-            </div>
-            <div class="d-flex justify-content-between">
-              <h2>تكاليف التاسيس والانشاء</h2>
-              <h2>{{ setupCostsComputed }}</h2>
-            </div>
-            <div class="row py-4 border-bottom">
-              <div class="col-md-6">
-                نوع التكلفة
+              <h5>{{ `اخر تحديث في` }}</h5>
+              <h5>{{ timestamp }}</h5>
+              <hr />
+              <div class="d-flex justify-content-between">
+                <h3>قائمة الملاك</h3>
+                <h3>{{ owners.length }}</h3>
               </div>
-              <div class="col-md-6">القيمة</div>
-              <div class="col-md-6">العمر الافتراضي</div>
-              <div class="col-md-6">الاستهلاك السنوي</div>
-            </div>
-            <div v-if="setupCosts.length > 0">  
-              <b-table striped hover :items="setupCosts"></b-table>
-            </div>
-            <h5 class="mt-4" v-else>لايوجد بيانات حتى الان</h5>
+              <table class="table">
+                <thead>
+                  <th>الاسم</th>
+                  <th>الحصة</th>
+                </thead>
+                <tbody>
+                  <tr v-for="(owner, index) in owners" :key="index">
+                    <td>{{ owner.name }}</td>
+                    <td>{{ owner.percentage }}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div class="page-break"></div>
+              <div class="py-4">
+                <h1 class="text-center">
+                  {{ marketInfo.name }}<br />حجم تداول سنوي{{
+                    marketInfo.tradingVolume
+                  }}
+                </h1>
+                <div class="mt-4">
+                  <h3>نبذة عن السوق</h3>
+                  <p>{{ marketInfo.description }}</p>
+                  <h3>المنتجات او الخدمات المقدمة</h3>
+                  <p>{{ marketInfo.briefOfYourProducts }}</p>
+                  <h3>الشريحة المستهدفة</h3>
+                  <p>{{ marketInfo.briefOfTargetAudience }}</p>
+                  <h3>اعتماد السوق والموردون</h3>
+                  <p>{{ marketInfo.briefOfVendors }}</p>
+                  <h3>من الخطط التسويقية</h3>
+                  <p>{{ marketInfo.briefOfYourMarketingPlans }}</p>
+                  <h3>المنافسة</h3>
+                  <p>{{ marketInfo.competitionAnalysis }}</p>
+                </div>
+              </div>
+              <div class="page-break"></div>
+              <div class="py-4">
+                <div class="d-flex justify-content-between">
+                  <h1>المنافسون في السوق</h1>
+                  <h1>{{ marketInfo.competitors.length }}</h1>
+                </div>
+                <table class="table">
+                  <thead>
+                    <th>اسم المنافس</th>
+                    <th>الحصة السوقية</th>
+                  </thead>
+                  <tbody>
+                    <tr
+                      v-for="(competitor, index) in marketInfo.competitors"
+                      :key="index"
+                    >
+                      <td>{{ competitor.name }}</td>
+                      <td>{{ competitor.share }}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div class="page-break"></div>
+              <div class="py-4">
+                <h1 class="text-center">تحليل SWOT</h1>
+                <div class="mt-4">
+                  <h1>نقاط القوة</h1>
+                  <p>{{ strengthPoints }}</p>
+                  <h1>نقاط الضعف</h1>
+                  <p>{{ weaknessPoints }}</p>
+                  <h1>الفرص المتاحة</h1>
+                  <p>{{ opportunities }}</p>
+                  <h1>التهديدات المتوقعة</h1>
+                  <p>{{ threats }}</p>
+                </div>
+              </div>
+              <div class="page-break"></div>
+              <div class="py-4">
+                <div class="d-flex justify-content-between">
+                  <h1>تكاليف التاسيس والانشاء</h1>
+                  <h1>
+                    {{ `${setupCostsComputed} ${accountingOptions.currency}` }}
+                  </h1>
+                </div>
+                <table class="table w-100">
+                  <thead>
+                    <th>نوع التكلفة<br />(العمر الافتراضي)</th>
+                    <th>القيمة<br />(% الاستهلاك السنوي)</th>
+                  </thead>
+                  <tbody>
+                    <tr v-for="(setupCost, index) in setupCosts" :key="index">
+                      <td>{{ setupCost.lifeSpan }}</td>
+                      <td>{{ setupCost.consumptionRatio }}</td>
+                    </tr>
+                    <h4 v-if="setupCosts.length === 0">
+                      لايوجد بيانات حتى الان
+                    </h4>
+                  </tbody>
+                </table>
+              </div>
+              <div class="page-break"></div>
+              <div class="py-4">
+                <div class="d-flex justify-content-center">
+                  <h1>الموظفين</h1>
+                  <h1>{{ salaries }}</h1>
+                </div>
+                <div class="mt-4">
+                  <table class="table w-100">
+                    <thead>
+                      <th>العدد X المسمى الوظيفي</th>
+                      <th>راتب شهري</th>
+                    </thead>
+                    <tbody>
+                      <tr v-for="(employee, index) in employees" :key="index">
+                        <td>
+                          {{
+                            `${employee.numberOfEmployees} X ${employee.jobTitle}`
+                          }}
+                        </td>
+                        <td>{{ employee.salary }}</td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+              <div class="page-break"></div>
+              <div class="py-4">
+                <div class="d-flex justify-content-between">
+                  <h1>المديونيات</h1>
+                  <h1>{{ debtsComputed }}</h1>
+                </div>
+                <div class="mt-4">
+                  <table class="table w-100">
+                    <thead>
+                      <th>الجهة الممولة<br>(الفائدة السنوية - سنوات السداد)</th>
+                      <th>القيمة<br>(المديونية)</th>
+                    </thead>
+                    <tbody>
+                      <tr v-for="(debt, index) in debts" :key="index">
+                        <td>
+                          {{ debt.loaner }}<br>
+                          {{ `${debt.APR} - ${debt.loanYears} سنوات` }}
+                        </td>
+                        <td>
+                          {{ debt.cost }}<br>
+                          {{ getTotalAPR(debt)}}
+                        </td>
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
+              </div>
             </div>
           </section>
         </vue-html2pdf>
@@ -690,6 +804,35 @@ export default {
     VueHtml2pdf,
   },
   computed: {
+    personalFinance: {
+      get() {
+        let result = this.capital - this.setupCostsComputed;
+        if (result > 0) result = 0;
+        else result = this.setupCostsComputed - this.capital;
+        return result;
+      },
+    },
+    debtsFinanced: {
+      get() {
+        return this.debts.reduce((a, b) => {
+          let annualCash = parseFloat(b.cost) * (parseFloat(b.APR) / 100);
+          let result =
+            parseFloat(b.cost) +
+            parseFloat(annualCash) * parseFloat(b.loanYears);
+          console.log(
+            `b.cost is: ${b.cost}, annualCash: ${annualCash}, b.loanYears: ${b.loanYears}`
+          );
+          console.log(parseFloat(annualCash) * parseFloat(b.loanYears));
+          return a + result;
+        }, 0);
+      },
+    },
+    capital: {
+      get() {
+        const sum = (a, b) => parseInt(a) + parseInt(b.cost);
+        return this.debts.reduce(sum, 0);
+      },
+    },
     setupCostsComputed: {
       get() {
         const sum = (a, b) => parseInt(a) + parseInt(b.cost);
@@ -766,7 +909,7 @@ export default {
       products: [],
     };
   },
-  mounted(){
+  mounted() {
     this.getNow();
   },
   methods: {
@@ -792,7 +935,7 @@ export default {
       this.setupCosts.splice(index, 1);
     },
     addDebt() {
-      this.debts.push({ loaner: "", cost: 0, APR: "", loanYears: "" });
+      this.debts.push({ loaner: "", cost: 0, APR: 0, loanYears: 0 });
     },
     removeDebt(index) {
       this.debts.splice(index, 1);
@@ -828,6 +971,12 @@ export default {
     removeProduct(index) {
       this.products.splice(index, 1);
     },
+    getProfitMargin(product) {
+      return (product.price - product.rawCost) * 100;
+    },
+    getTotalAPR(debt){
+      return (parseFloat(debt.cost) * (parseFloat(debt.APR) / 100)) * parseFloat(debt.loanYears); 
+    },
     getNow: function () {
       const today = new Date();
       const date =
@@ -850,5 +999,8 @@ export default {
 <style scoped>
 .card {
   text-align: right;
+}
+.page-break {
+  page-break-after: always;
 }
 </style>
